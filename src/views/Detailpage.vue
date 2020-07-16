0a968391b011679e4d18ae9cebb75af6973ddf60
laphatradap <laphatradap@disroot.org>
2020-07-16 16:36:07 +0200

    Setup folder structure, templates

---
+++ src/views/Detailpage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{nan}}</div>
+</template>
+
+<script>
+import { Vue, Component } from "vue-property-decorator";
+// import users from "@/store/modules/users";
+@Component
+export default class Detailpage extends Vue {
+  nan = "this is detail page";
+  // get username() {
+  //   return users.username;
+  // }
+}
+</script>
